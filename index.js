4a49a4ebf3347d351e330d27e27b15d2d92c1bad
Abnnerr <jonnas.ab.00@gmail.com>
2025-04-29 11:45:07 -0300

    update

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-ff
+
+// let soma = 0
+// for (let num = 1; num <= 100; num++) { 
+//     console.log(soma += num)
+// }
+
+
+// for(let num = 1; num <= 50; num++) {
+
+//     if (num % 2 == 0) {
+//         console.log(num)
+//     }
+// }
+
+
+let lista = ['jonas', 'abner', 'silva', 'vieira']
+
+
+for(let num = 0; num < lista.length; num++) {
+    console.log(lista[i])
+}
